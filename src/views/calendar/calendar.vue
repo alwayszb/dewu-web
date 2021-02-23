f0c5e144c6ef699c3c8bc54f927b1caececb0f44
zhoubin <zhoubin@ewell.cc>
2021-02-23 11:11:50 +0800

    feat(calendar): support mutiple products display

--- src/views/calendar/calendar.vue
+++ src/views/calendar/calendar.vue
@@ -18,23 +18,32 @@ export default {
         this.releaseProducts = data;
       });
     },
-    renderProductInfo(product) {
-      return (
-        <div style={{ textAlign: 'center' }}>
-          <img v-lazy={product.image} alt={product.name} height="60" width="auto" />
-          <div style={{ marginTop: '0.125rem', color: '#999', fontSize: '80%' }}>
-            {product.sellDate}
+    renderProductInfo(products) {
+      const getItem = (product, height = 60) => {
+        return (
+          <div style={{ display: 'flex', marginBottom: '0.5rem' }}>
+            <img v-lazy={product.image} alt={product.name} height={height} />
+            <div style={{ marginLeft: '0.125rem', fontSize: '80%' }}>
+              <div style={{ color: '#666' }}>{product.articleNumber}</div>
+              <div style={{ color: '#999' }}>{product.sellDate}</div>
+            </div>
           </div>
-        </div>
-      );
+        );
+      };
+      return products.map((product) => getItem(product));
     },
   },
   render() {
     const scopedSlots = {
       dateCellRender: (moment) => {
         const date = moment.format('YYYY.MM.DD');
-        const product = this.releaseProducts.find(({ sellDate }) => sellDate === date);
-        return product ? this.renderProductInfo(product) : null;
+        const products = this.releaseProducts.filter(({ sellDate }) => sellDate === date);
+        return products.length > 0 ? (
+          <div>
+            <div style={{ color: '#666', fontSize: '80%' }}>Total: {products.length}</div>
+            {this.renderProductInfo(products)}
+          </div>
+        ) : null;
       },
     };
 
